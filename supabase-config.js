3261eb9a1cdd44282e12f1cfa96d1dcd47cf0312
Bondarenco Andrei <upodpiscikov@gmail.com>
2026-06-07 19:12:14 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://kqzowoligcegduiabsxg.supabase.co/rest/v1/",
+  url: "https://kqzowoligcegduiabsxg.supabase.co",
   anonKey: "sb_publishable_Dj19571iny8hRgx18kCeaw_PowRBs5Y"
 };
